a78aaab0be884a5c4d2befd61ef540514c236310
lydiathornton <lydia.d.thornton@gmail.com>
2019-07-08 15:24:26 -0700

    assignments hello through list comprehension complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -1,8 +1,4 @@
-"""
-Python provides a number of ways to perform printing. Research
-how to print using the printf operator, the `format` string 
-method, and by using f-strings.
-"""
+
 
 x = 10
 y = 2.24552
@@ -11,7 +7,13 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("she was %s years old when she said %s in front of %s people" % (x, z, y))
 
 # Use the 'format' string method to print the same thing
 
+print("she was {} years old when she said {} in front of {} people".format(x, z, y))
+
 # Finally, print the same thing using an f-string
+
+
+print("she was %s years old when she said %s in front of %s people" % (x, z, y))
